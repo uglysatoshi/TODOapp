a107825c39f9bac1adc36ab15a64856cea71eee2
Satoshi <uglysatoshi@ya.ru>
2023-11-02 02:24:52 +0300

    setting up mongoDB access

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017"
+
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
 
 from application import routes
