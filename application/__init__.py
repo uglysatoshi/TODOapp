9dd7c820ab4d6f840b81c14b7935001e3bfcab76
Satoshi <uglysatoshi@ya.ru>
2023-11-03 21:31:05 +0300

    correcting db url, added POST method

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017"
+app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/db"
 app.config["SECRET_KEY"] = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
 
 mongodb_client = PyMongo(app)
--- application/routes.py
+++ application/routes.py
@@ -1,14 +1,30 @@
-from application import app
-from flask import render_template
+from application import app, db
+from flask import render_template, flash, request, redirect
 from .forms import TodoForm
+from datetime import datetime
 
 
 @app.route("/")
 def index():
-    return render_template("layout.html")
+    return render_template("view_todos.html")
 
 
-@app.route("/add_todo")
+@app.route("/add_todo", methods=["POST", "GET"])
 def add_todo():
-    form = TodoForm()
-    return render_template("add_todo.html", form=form)
+    if request.method == "POST":
+        form = TodoForm(request.form)
+        todo_name = form.name.data
+        todo_description = form.description.data
+        todo_completed = form.completed.data
+
+        db.todo_flask.insert_one({
+            "name": todo_name,
+            "description": todo_description,
+            "completed": todo_completed,
+            "date_completed": datetime.now()
+        })
+        flash("Your TODO is successfully inserted", "success")
+        return redirect("/")
+    else:
+        form = TodoForm()
+        return render_template("add_todo.html", form=form)
