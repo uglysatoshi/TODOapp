a3e50ac26ee99ecf55d9fd65a0d9d8d8fc5fdacb
Satoshi <uglysatoshi@ya.ru>
2023-11-04 03:24:55 +0300

    deleted sha key

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/db"
-app.config["SECRET_KEY"] = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+app.config["SECRET_KEY"] = ""
 
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
